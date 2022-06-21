b732995f6471be304b665ab82561a85d11edec29
TheSamuelVitor <71853407+TheSamuelVitor@users.noreply.github.com>
2022-06-21 11:58:25 -0300

    classes 4 to 6

---
+++ aula05/variaveis.js
@@ -0,0 +1,28 @@
+// definicao de variavel
+// var [nomeVariavel] = [dadoRecebido]
+// há diferença entre letra maiúscula e minúscula
+
+// tipos primitivos primordiais:
+// number: podendo ser tanto int como float
+// string: definicao entre aspas
+// boolean: true ou false
+// null: nulo
+// undefined: indefinido
+// object/array: pode ser considerado uma variavel por possuir uma definição
+// function:  nem sempre é considerado uma variável, mas é interessante fazer
+var n1 = 5
+var n2 = 8.5
+var n3 = 15
+var s1 = "Javascript"
+var s2 = "Curso em video"
+var s3 = "Guanabara"
+var sim = true
+
+// imprimindo no terminal os tipos de cada variavel
+console.log(typeof(n1))
+console.log(typeof(n2))
+console.log(typeof(n3))
+console.log(typeof(s1))
+console.log(typeof(s2))
+console.log(typeof(s3))
+console.log(typeof(sim))
---
+++ aula06/ex002.js
@@ -0,0 +1,21 @@
+var nome = 'Samuel'
+console.log("Meu nome é", nome)
+
+// para formatacao do print usa-se a crase
+var idade = 21
+var nota = 6.5
+console.log(`O aluno ${nome} de ${idade} anos possui nota igual ${nota}`)
+
+var conteudo = 'Javascript'
+
+// para deixar em letra maiuscula
+console.log(conteudo.toUpperCase())
+
+// para deixar em letra maiuscula
+console.log(conteudo.toLowerCase())
+
+// retorna com o elemento da posicao dada
+console.log(conteudo.charAt(1))
+
+// Localizacao de um determinado elemento
+console.log(conteudo.indexOf('J'))
