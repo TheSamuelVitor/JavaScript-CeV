7795427564b701951d717c36b5bb8e47df6b32ad
TheSamuelVitor <71853407+TheSamuelVitor@users.noreply.github.com>
2022-07-18 10:53:06 -0300

    feat: file organization

---
+++ Curso/aula01/script.js
@@ -0,0 +1,10 @@
+let idade = 18
+console.log(idade)
+idade = 17
+console.log(idade)
+
+// const apenas se não for constante
+const ano = 2022
+console.log(ano)
+ano = 2023
+console.log(ano)
---
+++ Curso/aula02/script.js
@@ -0,0 +1,9 @@
+let nome = 'Rafael'
+let idade = 25
+let estarAprovado = true
+
+if (estarAprovado == true){
+    console.log("Aprovado")
+}else{
+    console.log("Reprovado")
+}
